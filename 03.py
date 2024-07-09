436e9394f8432348379b1bd6fbfd7fac5e620ab3
solver941 <marek.heckelmoser@seznam.cz>
2024-07-09 10:38:53 +0200

    test

---
+++ 03.py
@@ -0,0 +1,26 @@
+#1-3
+age = 20
+height = 185.4
+complex_number = 5+3j
+
+#4
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+area = base * height / 2
+
+print("The area of triangle is "+str(area))
+
+#5
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+print("Perimeter of the triangle is "+str(a+b+c))
+
+
+#6
+length = float(input("Enter lenght: "))
+width = float(input("Enter width: "))
+
+print("Area of the triangle is "+str(length*width))
+print("Perimeter of the triangle is "+str(2 * (length+width)))
